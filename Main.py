8ec4f02b88e67e567467a15c21850d917e9872fb
Grant <LFGSaito@users.noreply.github.com>
2018-10-08 13:19:15 -0400

    I knew I forgot something.

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(activity=discord.Game(name='with access codes'))
+    await bot.change_presence(activity=discord.Game(name='football.'))
 
 @bot.command(aliases=['about'])
 async def info(ctx):
