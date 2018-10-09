ffedfc806c7b49cdd9f5de3cc90825d558cc64cb
Grant <LFGSaito@users.noreply.github.com>
2018-10-09 09:51:29 -0400

    REEEE

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(activity=discord.Game(name='with IClickers.'))
+    await bot.change_presence(activity=discord.Game(name='owl help | IClickers!'))
 
 @bot.command(aliases=['about'])
 async def info(ctx):
