ed84aa2510261f7cf8ddf951bb0295ca459b85e1
Grant <LFGSaito@users.noreply.github.com>
2018-10-17 10:26:32 -0400

    TEST

--- Main.py
+++ Main.py
@@ -124,7 +124,7 @@ async def tex(ctx, *, tex : str):
 
 @bot.command()
 async def restart(ctx):
-    if any([int(x.id) in secrets['exitp'] for x in ctx.author.id]):
+    if any([int(x.id) in secrets['exitperson'] for x in ctx.author.id]):
         await ctx.channel.send("Restarting qrm...")
         await bot.logout()
     else:
@@ -135,7 +135,7 @@ async def restart(ctx):
 
 @bot.command()
 async def shutdown(ctx):
-    if any([int(x.id) in secrets['exitp'] for x in ctx.author.id]):
+    if any([int(x.id) in secrets['exitperson'] for x in ctx.author.id]):
         await ctx.channel.send("Shutting down qrm...")
         os._exit(42)
     else:
