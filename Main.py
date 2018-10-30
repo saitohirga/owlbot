7c23c574e89f74cf855b3144c68ece88330acb13
Grant <LFGSaito@users.noreply.github.com>
2018-10-30 15:14:13 -0400

    test?

--- Main.py
+++ Main.py
@@ -41,21 +41,25 @@ async def on_ready():
     print('------')
     await bot.change_presence(activity=discord.Game(name='owl help | IClickers!'))
 
+config = {}
+with open('config_default.json', 'r') as f:
+    config = json.load(f)
+    print('config loaded')
 
-@bot.event
-async def on_message(message):
-        # make case-insensitive
-        message.content = message.content.lower()
-
-        # get the bonks, boonks, and the oofs
-        # TODO: Make a thread that periodically saves the oof count
-        if message.content == 'oof':
-            config['oofs'] += 1
-            await message.channel.send('rip')
-        elif message.content == 'bonk':
-            await message.channel.send(bonk)
-        elif message.content.startswith('boonk'):
-            await message.channel.send(boonk)
+#@bot.event
+#async def on_message(message):
+#        # make case-insensitive
+#        message.content = message.content.lower()
+#
+#        # get the bonks, boonks, and the oofs
+#        # TODO: Make a thread that periodically saves the oof count
+#        if message.content == 'oof':
+#            config['oofs'] += 1
+#           await message.channel.send('rip')
+#        elif message.content == 'bonk':
+#            await message.channel.send(bonk)
+#        elif message.content.startswith('boonk'):
+#            await message.channel.send(boonk)
 
 
 @bot.command(aliases=['about'])
@@ -264,10 +268,6 @@ with open('secrets.json') as secrets_file:
 
 WORDS = open('resources/words').read().lower().splitlines()
 
-config = {}
-with open('config_default.json', 'r') as f:
-    config = json.load(f)
-    print('config loaded')
 
 bonk = (':regional_indicator_b: '
         ':regional_indicator_o: '
