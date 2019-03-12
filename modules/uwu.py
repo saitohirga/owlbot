c79de2e3b08ec61e9f9145edbd1c15faed65a132
Saito Hiriga <LFGSaito@users.noreply.github.com>
2019-03-11 23:00:34 -0400

    Let try this!

--- modules/uwu.py
+++ modules/uwu.py
@@ -5,7 +5,7 @@ import key
 
 @client.message()
 async def hunter_irl(message: discord.Message):
-	if (message.guild.id == 364480908528451584) and ("uwu" in message.content.lower().replace(" ", "")):
+	if (message.guild.id == 458765854624972811) and ("uwu" in message.content.lower().replace(" ", "")):
 		try:
 			await message.add_reaction(discord.utils.find(lambda x: x.id == key.uwu_emoji, message.guild.emojis))
 		except:  # thats ok, this is only a fun little joke
@@ -13,8 +13,8 @@ async def hunter_irl(message: discord.Message):
 
 @client.message()
 async def ntoskrnl_irl(message: discord.Message):
-	if (message.guild.id == 364480908528451584) and "<:ntoskrnl_irl:486325601750351882>" in message.content:
+	if (message.guild.id == 458765854624972811) and "<:ntoskrnl_irl:486325601750351882>" in message.content:
 		try:
-			await message.add_reaction(discord.utils.find(lambda x: x.id == 486325601750351882, message.guild.emojis))
+			await message.add_reaction(discord.utils.find(lambda x: x.id == 459861613197918230, message.guild.emojis))
 		except:
 			pass
