a793b99b7635691601ef2f85b82388430fdce41a
Grant Baron <LFGSaito@users.noreply.github.com>
2019-03-15 14:40:59 -0400

    Test!

---
+++ modules/xkcd.py
@@ -0,0 +1,24 @@
+from client import client
+
+import discord
+
+
+cmd_name = "x"
+
+client.basic_help(title=cmd_name, desc="Pulls up a xkcd.")
+
+detailed_help = {
+	"Usage": f"{client.default_prefix}{cmd_name}",
+	"Arguments": "None",
+	"Description": "Pulls up a xkcd, all you need is a number!",
+	# NO Aliases field, this will be added automatically!
+}
+client.long_help(cmd=cmd_name, mapping=detailed_help)
+
+
+@client.command(trigger=cmd_name,
+				aliases=[])  # aliases is a list of strs of other triggers for the command
+async def command(command: str, message: discord.Message):
+
+	await message.channel.send('http://xkcd.com/' + message)
+	return
