6569a3bd95b8307d58072128687d87a83d98ff69
Grant Baron <LFGSaito@users.noreply.github.com>
2019-03-17 04:04:02 -0400

    ??????

--- modules/xkcd.py
+++ modules/xkcd.py
@@ -19,7 +19,7 @@ client.long_help(cmd=cmd_name, mapping=detailed_help)
 @client.command(trigger=cmd_name,
 				aliases=[])  # aliases is a list of strs of other triggers for the command
 async def command(command: str, message: discord.Message):
- num = command[1:]
+ num = command[2:]
  await message.channel.send('http://xkcd.com/' + num)
 
  return
