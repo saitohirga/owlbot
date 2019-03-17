8ebd8cbc46b5d671b1a31301e4ebf57e7c59dfd0
Grant Baron <LFGSaito@users.noreply.github.com>
2019-03-17 03:41:49 -0400

    BOONK

---
+++ modules/boonk.py
@@ -0,0 +1,22 @@
+from client import client
+import discord
+
+
+@client.message()
+async def boonk(message: discord.Message):
+
+	htm_boonk = (':regional_indicator_b: '
+				 ':regional_indicator_o: '
+				 ':regional_indicator_o: '
+				 ':regional_indicator_n: '
+				 ':regional_indicator_k: '
+				 ':regional_indicator_g: '
+				 ':regional_indicator_a: '
+				 ':regional_indicator_n: '
+				 ':regional_indicator_g:')
+
+	if message.content.lower().endswith("boonk"):
+		try:
+			await message.channel.send(htm_boonk)
+		except:  # that's fine, we don't care
+			pass
