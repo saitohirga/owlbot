297c99dbd01a561b98e803738ee1e26ae2300a63
Grant Baron <LFGSaito@users.noreply.github.com>
2019-03-17 04:18:35 -0400

    :D

--- Main.py
+++ Main.py
@@ -5,28 +5,74 @@ from key import token
 # To load new modules, copy/paste the line below, uncommented, with X filled in for the name of your file
 # from modules import X
 
+# noinspection PyUnresolvedReferences
+
 from modules import _debug
+# noinspection PyUnresolvedReferences
+
 from modules import about
+# noinspection PyUnresolvedReferences
+
 from modules import emoji_stats
+# noinspection PyUnresolvedReferences
+
 from modules import exec
+# noinspection PyUnresolvedReferences
+
 from modules import exit
+# noinspection PyUnresolvedReferences
+
 from modules import info
+# noinspection PyUnresolvedReferences
+
 from modules import markov
+# noinspection PyUnresolvedReferences
+
 from modules import message_log
+# noinspection PyUnresolvedReferences
+
 from modules import morse
+# noinspection PyUnresolvedReferences
+
 from modules import music
+# noinspection PyUnresolvedReferences
+
 from modules import nou
+# noinspection PyUnresolvedReferences
+
 from modules import ping
+# noinspection PyUnresolvedReferences
+
 from modules import pingreact
+# noinspection PyUnresolvedReferences
+
 from modules import stats
+# noinspection PyUnresolvedReferences
+
 from modules import time
+# noinspection PyUnresolvedReferences
+
 from modules import units
+# noinspection PyUnresolvedReferences
+
 from modules import uwu
+# noinspection PyUnresolvedReferences
+
 from modules import help
+# noinspection PyUnresolvedReferences
+
 from modules import oof
+# noinspection PyUnresolvedReferences
+
 from modules import xkcd
+# noinspection PyUnresolvedReferences
+
 from modules import boonk
+# noinspection PyUnresolvedReferences
+
 from modules import bonk
+# noinspection PyUnresolvedReferences
+
 from modules import phonetics
 
 
