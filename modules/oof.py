354fc586bc7a2e561b060929b97082b5d3bf3d20
Grant Baron <LFGSaito@users.noreply.github.com>
2019-04-05 12:46:18 -0400

    oof

--- modules/oof.py
+++ modules/oof.py
@@ -4,7 +4,7 @@ import discord
 
 @client.message()
 async def oof(message: discord.Message):
-	if message.content.lower().endswith("oof"):
+	if message.content.lower()("oof"):
 		try:
 			await message.channel.send("rip")
 		except:  # that's fine, we don't care
