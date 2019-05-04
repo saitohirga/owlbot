36e62b041a8c3ea6c10abc9b00c4df6c155ef403
SaitoH <LFGSaito@users.noreply.github.com>
2019-05-04 15:55:11 -0400

    Update oof.py

--- modules/oof.py
+++ modules/oof.py
@@ -1,11 +1,13 @@
 from client import client
 import discord
+
 oofs = 0
 
 @client.message()
 async def oof(message: discord.Message):
     if message.content.lower().endswith("oof"):
         try:
+            global oofs
             oof.oofs += 1
             await message.channel.send("rip")
         except:
