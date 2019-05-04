abedb2e3498119bcca12cdc797c7d72a0542b7fb
SaitoH <LFGSaito@users.noreply.github.com>
2019-05-04 15:56:28 -0400

    sigh

--- modules/oof.py
+++ modules/oof.py
@@ -7,7 +7,6 @@ oofs = 0
 async def oof(message: discord.Message):
     if message.content.lower().endswith("oof"):
         try:
-            global oofs
             oof.oofs += 1
             await message.channel.send("rip")
         except:
