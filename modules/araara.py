f7290e925e58618f0fec3daa4fefbd95e113611c
SaitoH <LFGSaito@users.noreply.github.com>
2019-11-04 15:06:42 -0500

    Update araara.py

--- modules/araara.py
+++ modules/araara.py
@@ -6,6 +6,6 @@ import discord
 async def oof(message: discord.Message):
     if message.content.lower().endswith("ara ara"):
         try:
-            await message.channel.send("https://steamuserimages-a.akamaihd.net/ugc/928174091350767352/781BA205BF32A445ABA7993EF7F16751F2B9E312/")
+            await message.channel.send("http://giphygifs.s3.amazonaws.com/media/4SSj1rY2T1Eju/giphy.gif")
         except:
             pass
