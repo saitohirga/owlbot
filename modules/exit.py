fb1ee6fe28c45f5a8fdae4915c5eb2b0aefc43f7
SaitoH <LFGSaito@users.noreply.github.com>
2019-11-07 14:43:41 -0500

    Update exit.py

--- modules/exit.py
+++ modules/exit.py
@@ -15,9 +15,11 @@ import time
 @client.command(trigger="kill", aliases=["exit"])
 async def command(command: str, message: discord.Message):
 	if message.author.id != shutdown_user:
-		await message.add_reaction("❌")
-		if message.author.id == key.shutdown_easter_egg_user:
+		await __common__.failure(message)
+		if message.author.id == 269154594863579138:
 			await message.channel.send("*hehehe*\n\nCan't fool me! >:3")
+		if message.author.id == 427539762929336320:
+			await message.channel.send("no u")
 		return
 	else:
 		parts = command.split(" ")
@@ -35,7 +37,7 @@ async def command(command: str, message: discord.Message):
 										f"Hostname: {gethostname()}\n"
 										f"Uptime: {uptime/86400:.4f} days\n"
 										f"To confirm shutdown, react to this message with ☑ in 30 seconds")
-			await m.add_reaction("☑")
+			await __common__.confirm(m)
 
 			def reaction_check(reaction, user):
 				return (reaction.message.id == m.id and
@@ -45,18 +47,18 @@ async def command(command: str, message: discord.Message):
 			try:
 				await client.wait_for("reaction_add", check=reaction_check, timeout=30)
 			except asyncio.TimeoutError:
-				await m.add_reaction("❌")
+				await __common__.failure(m)
 				await m.remove_reaction("☑", m.guild.me)
-				await message.add_reaction("❌")
+				await __common__.failure(message)
 				await m.edit(content=m.content.replace("To confirm shutdown, react to this message with ☑ in 30 seconds", "Shutdown timed out. Deleting this message in 30 seconds."), delete_after=30)
 				return
 			else:
 				client.active = False
 				await m.delete()
-				await message.add_reaction("☑")
+				await __common__.confirm(message)
 				await message.channel.send("Shutting down bot...")
 				await message.channel.send(f"Uptime: { time.perf_counter() - client.first_execution:.3f} seconds ({(time.perf_counter() - client.first_execution) / 86400:.3f} days)")
 				await asyncio.sleep(0.1)  # give the above a chance to do its thing
 				log.info(f"Bot shutdown initiated at {datetime.utcnow().__str__()} by {message.author.name}#{message.author.discriminator}")
-				await client.on_shutdown()
+				await client.do_shutdown()
 	return
