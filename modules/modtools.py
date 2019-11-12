c2bcb43a921a4d7a6ef424031c7693aa3bdf3919
SaitoH <LFGSaito@users.noreply.github.com>
2019-11-12 14:59:49 -0500

    Update modtools.py

--- modules/modtools.py
+++ modules/modtools.py
@@ -203,5 +203,4 @@ async def help_mods(command: str, message: discord.Message):
                                   "default 25). Maximum purge size is 100 messages.") 
     if admin_role in message.author.roles:
         await message.channel.send(embed=embed)
-    else 
-        await message.channel.send("Not a admin, shoo shoo")
+  
