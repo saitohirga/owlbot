2f57cba30c4e0ec2e4fa4a787ebff3073a352390
Grant Baron <LFGSaito@users.noreply.github.com>
2019-11-19 18:01:01 -0500

    Rat's in the Caf?

--- modules/rat.py
+++ modules/rat.py
@@ -2,23 +2,22 @@ from client import client
 
 import discord
 
-
 cmd_name = "rat"
 
 client.basic_help(title=cmd_name, desc="Has rats been spotted in the caf?.")
 
 detailed_help = {
-	"Usage": f"{client.default_prefix}{cmd_name}",
-	"Arguments": "None",
-	"Description": "Any Rats in the caf?",
-	# NO Aliases field, this will be added automatically!
+    "Usage": f"{client.default_prefix}{cmd_name}",
+    "Arguments": "None",
+    "Description": "Any Rats in the caf?",
+    # NO Aliases field, this will be added automatically!
 }
 client.long_help(cmd=cmd_name, mapping=detailed_help)
 
 
 @client.command(trigger=cmd_name,
-				aliases=[])  # aliases is a list of strs of other triggers for the command
-async def command(command: str, message: discord.Message):
-	rat = 'No rats spotted in the caf as of today, if this changes DM Saito'
+                aliases=[])  # aliases is a list of strs of other triggers for the command
+async def rat(command: str, message: discord.Message):
+    rat = 'No rats spotted in the caf as of today, if this changes DM Saito'
     await message.channel.send(rat)
-	return
+    return
