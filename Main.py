8b51493da0d422c5c65801b556c7b8fbae2e12aa
SaitoH <2722492+saitohirga@users.noreply.github.com>
2021-09-01 18:57:40 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from client import client
-from data.key import token
-
+from key import token
+    
 # To load new modules, copy/paste the line below, uncommented, with X filled in for the name of your file
 # from modules import X
 
